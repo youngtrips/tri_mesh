6374379eceeb97f8d95f73a23043a7a4a554088d
youngtrips <youngtrips@163.com>
2011-12-22 16:56:02 +0800

    update

--- queue.c
+++ queue.c
@@ -9,89 +9,53 @@
 
 #include "queue.h"
 
-queue_t *queue_init() 
+
+#define INIT_CHUNK_SIZE 16
+typedef struct queue_chunk_t {
+    size_t size;
+    size_t head;
+    size_t tail;
+    dlist_t node;
+    void *chunk[1];
+} queue_chunk_t;
+
+struct queue_t {
+    size_t chunk_num;
+    dlist_t chunk_list;
+    queue_chunk_t *rpos;
+    queue_chunk_t *wpos;
+};
+
+queue_t *queue_init()
 {
-    queue_t *que;
-
-    que = (queue_t*)malloc(sizeof(queue_t));
-    if(!que)
-        return NULL;
-    init_dlist_node(&(que->head));
-    que->size = 0;
-    return que;
 }
 
-int queue_push(queue_t *que, void *data) {
-    queue_entry_t *entry;
-    entry = (queue_entry_t*)malloc(sizeof(queue_entry_t));
-    if(!entry)
-        return -1;
-    entry->data = data;
-    init_dlist_node(&(entry->qnode));
-    dlist_add_tail(&(entry->qnode), &(que->head));
-    que->size++;
-    return 0;
+int queue_push(queue_t *que, void *data)
+{
 }
 
-void *queue_head(queue_t *que) {
-    queue_entry_t *entry;
-    dlist_t *p;
-
-    if(que->size == 0)
-        return NULL;
-    p = que->head.next;
-    entry = (queue_entry_t*)dlist_entry(p, queue_entry_t, qnode);
-    return entry->data;
+void *queue_head(queue_t *que)
+{
 }
 
-void *queue_tail(queue_t *que) {
-    queue_entry_t *entry;
-    dlist_t *p;
-
-    if(que->size == 0)
-        return NULL;
-    p = que->head.prev;
-    entry = (queue_entry_t*)dlist_entry(p, queue_entry_t, qnode);
-    return entry->data;
+void *queue_tail(queue_t *que)
+{
 }
 
-void *queue_pop(queue_t *que) {
-    void *res;
-
-    queue_entry_t *entry;
-    dlist_t *p;
-
-    res = queue_head(que);
-    if (res == NULL) {
-        return NULL;
-    }
-
-    p = que->head.next;
-    dlist_del(p);
-    entry = (queue_entry_t*)dlist_entry(p, queue_entry_t, qnode);
-    free(entry);
-    que->size--;
-    return res;
+void *queue_pop(queue_t *que)
+{
 }
 
-int queue_size(queue_t *que) {
-    return que->size;
+size_t queue_size(queue_t *que)
+{
 }
 
-int queue_empty(queue_t *que) {
-    return que->size == 0;
+int queue_empty(queue_t *que)
+{
 }
 
-void queue_destroy(queue_t *que) {
-    queue_entry_t *entry;
-    dlist_t *p;
-    dlist_t *q;
-    if(!que) return;
-    dlist_foreach_safe(p, q, &(que->head)) {
-        entry = (queue_entry_t*)dlist_entry(p, queue_entry_t, qnode);
-        free(entry);
-    }
-    free(que);
+void queue_destroy(queue_t *que)
+{
 }
 
 
